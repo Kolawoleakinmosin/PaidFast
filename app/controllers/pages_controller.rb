eaac95f509441ebb0118b40c9450b91d9ef7978b
Karmen de Reuck <dereuckkarmen@gmail.com>
2023-03-23 14:46:57 +0200

    qr work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,24 +7,18 @@ class PagesController < ApplicationController
   end
 
   def qr_generator
-
-    qrcode = RQRCode::QRCode.new("https://buy.stripe.com/test_bIYdS7gBY4KL2cg9AA")
+    @qrcode = RQRCode::QRCode.new("https://buy.stripe.com/test_bIYdS7gBY4KL2cg9AA")
 
     # NOTE: showing with default options specified explicitly
-    png = qrcode.as_png(
-      bit_depth: 1,
-      border_modules: 4,
-      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
-      color: "black",
-      file: nil,
-      fill: "white",
-      module_px_size: 6,
-      resize_exactly_to: false,
-      resize_gte_to: false,
-      size: 120
+    @svg = @qrcode.as_svg(
+      color: "000",
+      shape_rendering: "crispEdges",
+      module_size: 5,
+      standalone: true,
+      use_path: true
     )
-
-    IO.binwrite("/tmp/stripe-qrcode.png", png.to_s)
   end
 
+  def playground
+  end
 end
