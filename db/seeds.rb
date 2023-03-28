dd6999757318612f697126cb6a4c2065cd5303a2
Karmen de Reuck <dereuckkarmen@gmail.com>
2023-03-28 14:49:54 +0200

    updated seed file and finished /transactions page

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,9 @@ puts 'Creating 1500 fake transactions...'
 1500.times do
   transaction = Transaction.new(
     price_cents:    Faker::Number.within(range: 100..2000),
-    created_at: Faker::Date.between(from: '2023-01-23', to: Date.today),
-    user: User.first
+    user: User.first,
+    customer_name: Faker::Name.unique.name,
+    timestamp: Faker::Time.between_dates(from: Date.today - 120, to: Date.today, period: :day)
   )
   transaction.save!
 end
